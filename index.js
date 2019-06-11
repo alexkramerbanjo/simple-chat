d185a554c728da92021a614958c37e6dd5c65264
Alex Kramer <alexkrameris@gmail.com>
2019-06-11 17:59:46 -0400

    commit with gitignore

---
+++ index.js
@@ -0,0 +1,30 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+const fs = require("fs");
+const express = require("express");
+path = require("path");
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+io.on("connection", async function(socket) {
+  console.log("a user connected");
+  await fs.readFile("public/messages.txt", "utf8", (err, data) => {
+    if (err) throw err;
+    socket.emit("messages", data);
+  });
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+  socket.on("chat message", function(msg) {
+    io.emit("chat message", msg);
+    fs.appendFile("public/messages.txt", "\n" + msg, err => {
+      if (err) throw err;
+      console.log('The "data to append" was appended to file!');
+    });
+  });
+});
+
+http.listen(3000, function() {
+  console.log("listening on *:3000");
+});
